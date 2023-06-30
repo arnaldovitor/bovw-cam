5b828eff7ac0584d03df34690122a2ce7a4638fa
Arnaldo Vitor <arnaldovitorbarros@gmail.com>
2023-06-30 19:16:36 -0300

    tests: bovw simple tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import cv2
+import pandas as pd
 from pytest import fixture
 
 
@@ -15,3 +16,19 @@ def wrong_image_path():
 @fixture
 def dog_image():
     return cv2.imread('./tests/test_images/dog.png')
+
+
+@fixture
+def extracted_features():
+    data = {
+        'image_path': ['image_1.jpg', 'image_2.jpg', 'image_3.jpg', 'image_4.jpg', 'image_5.jpg'],
+        'keypoint_coord_x': [100, 200, 300, 400, 500],
+        'keypoint_coord_y': [50, 75, 100, 125, 150],
+        'target': [1, 0, 0, 1, 1],
+        'feature_1': [0.1, 0.2, 0.3, 0.4, 0.5],
+        'feature_2': [0.5, 0.4, 0.3, 0.2, 0.1],
+        'feature_3': [10, 20, 30, 40, 50],
+        'feature_4': [5, 10, 15, 20, 25]
+    }
+    
+    return pd.DataFrame(data)
---
+++ tests/test_bovw.py
@@ -0,0 +1,13 @@
+from src.bovw import BoVW
+
+
+def test_if_kmeans_is_instantiated_correctly(extracted_features):
+    bovw = BoVW(extracted_features, 2, 16)
+    assert len(bovw.mini_batch_kmeans.cluster_centers_) == 2
+
+
+def test_if_histograms_are_calculated_correctly(extracted_features):
+    bovw = BoVW(extracted_features, 2, 16)
+    single_image_features = extracted_features[extracted_features['image_path'] == 'image_1.jpg']
+    histogram, _ = bovw._get_single_histogram(single_image_features[['feature_1', 'feature_2', 'feature_3', 'feature_4']])
+    assert histogram in ([1, 0], [0, 1])
